6a6ea74f4e686dfe34aa5bc14dc3e1b7bbfd0feb
Tanjona RAKOTO <trakoto@laprovence.com>
2025-01-02 18:12:20 +0100

    first commit

---
+++ nav.js
@@ -0,0 +1,8 @@
+document.write(`
+    <nav>
+        <a href="index.html">Accueil</a>
+        <a href="services.html">Services</a>
+        <a href="tarifs.html">Tarifs</a>
+        <a href="contact.html">Contact</a>
+    </nav>
+    `);
